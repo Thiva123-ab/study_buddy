e1e83c77ca978773ca81e162c4aa64c989874ba5
Thivanka Tharuka <thivankatharuka36@gmail.com>
2026-07-01 16:03:26 +0530

    initialize Tailwind CSS design system and update Vite configuration with React and TanStack Start plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from "@tanstack/react-start/config";
+import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   server: { entry: "server" },
-  vite: {
-    plugins: [
-      tailwindcss(),
-      tsconfigPaths(),
-    ],
-  },
+  plugins: [
+    tanstackStart(),
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+  ],
 });
